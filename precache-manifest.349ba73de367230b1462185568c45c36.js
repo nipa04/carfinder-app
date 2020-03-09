c3bd74214d0f39c5ab3c12b39fa6352167671a27
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-09 02:39:03 +0000

    Deploy nipa04/carfinder-app to github.com/nipa04/carfinder-app.git:gh-pages

---
+++ precache-manifest.349ba73de367230b1462185568c45c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11745f5d9dcb6702570cce56ae755520",
+    "url": "/carfinder-app/index.html"
+  },
+  {
+    "revision": "58b28c62058775db28d6",
+    "url": "/carfinder-app/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "0b499ee389e31b1eb00b",
+    "url": "/carfinder-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "58b28c62058775db28d6",
+    "url": "/carfinder-app/static/js/2.2623be95.chunk.js"
+  },
+  {
+    "revision": "0b499ee389e31b1eb00b",
+    "url": "/carfinder-app/static/js/main.4129e14f.chunk.js"
+  },
+  {
+    "revision": "395cd8b981189962caf0",
+    "url": "/carfinder-app/static/js/runtime~main.aa09182b.js"
+  }
+]);
